68936f56d60ea48c855810d36b552789989337ff
matanswisa <matanswisa01@gmail.com>
2022-11-06 12:10:56 +0200

    add the ability to recognize faces

---
+++ public/_sketch.js
@@ -0,0 +1,222 @@
+const port = 5000;
+// import * as FACE_API from '../dist/face-api.esm.js';
+
+
+let faceapi;
+let detections = [];
+
+let video;
+let canvas;
+let face;
+
+// const faceOptions = {
+//   withLandmarks: true,
+//   withExpressions: true,
+//   withDescriptors: true,
+//   minConfidence: 0.5
+// };
+
+async function getLabelFaceDescriptions(labels = ['Matan', 'Yehuda', 'Yoni_Open']) {
+  // faceapi = await ml5.faceApi(video, faceOptions, faceReady);
+  return await Promise.all(
+    labels.map(async label => {
+      // fetch image data from urls and convert blob to HTMLImage element
+      const imgUrl = `./photos/${label}.jpg`
+      const img = await faceapi.model.fetchImage(imgUrl)
+
+      // detect the face with the highest score in the image and compute it's landmarks and face descriptor
+      const fullFaceDescription = await faceapi.model.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor()
+
+
+      if (!fullFaceDescription) {
+        throw new Error(`no faces detected for ${label}`)
+      }
+
+      const faceDescriptors = [fullFaceDescription.descriptor]
+      console.log(fullFaceDescription.descriptor);
+      console.log(faceDescriptors);
+      // alert('ff');
+      return new faceapi.model.LabeledFaceDescriptors(label, faceDescriptors)
+    })
+  )
+}
+
+async function loadFacesFromDB() {
+  return new Promise(async (resolve, reject) => {
+    const faces = await fetch(`http://localhost:${port}/getFaces`);
+    if (!faces && !faces.length) {
+      reject({ message: "Can't find faces" });
+    } else {
+      resolve(faces);
+    }
+  })
+}
+
+async function savePerson(face) {
+  const id = document.getElementById('id').value;
+  const name = document.getElementById('name').value;
+
+
+  const rawResponse = await fetch(`http://localhost:${port}/uploadFace`, {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      id: id,
+      name: name,
+      descriptors: face.descriptor,
+      parts: face.parts,
+    })
+  });
+  const content = await rawResponse.json();
+  console.log(content);
+
+}
+
+
+
+function setup() {
+
+  initUploadNewFaceButton();
+  loadFacesFromDB().then((res) => {
+    console.log("faces:", res);
+  }).catch((erro) => {
+    console.error(erro);
+  })
+  canvas = createCanvas(720, 480);
+  canvas.id("canvas");
+
+  video = createCapture(VIDEO);// Creat the video: ビデオオブジェクトを作る
+  video.id("video");
+  video.size(width, height);
+
+  const faceOptions = {
+    withLandmarks: true,
+    withExpressions: true,
+    withDescriptors: true,
+    minConfidence: 0.5
+  };
+
+
+  // Unfinished code , should do comparison with landmarks and descriptor of video and image
+  // const maxDescriptorDistance = 0.6;
+  faceapi = ml5.faceApi(video, faceOptions, faceReady);
+  // const faceMatcher = new faceapi.model.FaceMatcher();
+
+  //Initialize the model: モデルの初期化
+}
+
+let counterId = 0;
+const timeCounter = (start, timerHtml) => setInterval(function () {
+  let delta = Date.now() - start; // milliseconds elapsed since start
+  if (Math.floor(delta / 1000) === 6) {
+    clearInterval(counterId);
+    return;
+  };
+
+  timerHtml.innerHTML = Math.floor(delta / 1000)
+}, 1000); // update about every second
+
+function initUploadNewFaceButton() {
+  const timerHtml = document.getElementById('timer');
+  const btn = document.getElementById('new-person-btn');
+
+  btn.addEventListener('click', async (e) => {
+    if (face !== null && face) {
+      e.preventDefault();
+      counterId = timeCounter(Date.now(), timerHtml);
+      await savePerson(face);
+    }
+  })
+}
+
+let faces;
+
+function faceReady() {
+  getLabelFaceDescriptions().then((data) => {
+    faces = data;
+  })
+  faceapi.detect(gotFaces);// Start detecting faces: 顔認識開始
+}
+
+// Got faces: 顔を検知
+function gotFaces(error, result) {
+  if (error) {
+    console.log(error);
+    return;
+  }
+
+  detections = result;　//Now all the data in this detections: 全ての検知されたデータがこのdetectionの中に
+  face = detections.length ? detections[0] : null; //if there is at least one detection
+  // console.log(detections);
+
+
+
+  const maxDescriptorDistance = 0.6;
+  if (faces) {
+    const faceMatcher = new faceapi.model.FaceMatcher(faces, maxDescriptorDistance);
+    const recognitionResults = detections.map(fd => faceMatcher.findBestMatch(fd.descriptor));
+    console.log(recognitionResults);
+  }
+
+
+  clear();//Draw transparent background;: 透明の背景を描く
+  drawBoxs(detections);//Draw detection box: 顔の周りの四角の描画
+  drawLandmarks(detections);//// Draw all the face points: 全ての顔のポイントの描画
+  drawExpressions(detections, 20, 250, 14);//Draw face expression: 表情の描画
+
+  faceapi.detect(gotFaces);// Call the function again at here: 認識実行の関数をここでまた呼び出す
+}
+
+function drawBoxs(detections) {
+  if (detections.length > 0) {//If at least 1 face is detected: もし1つ以上の顔が検知されていたら
+    for (f = 0; f < detections.length; f++) {
+      let { _x, _y, _width, _height } = detections[f].alignedRect._box;
+      stroke(44, 169, 225);
+      strokeWeight(1);
+      noFill();
+      rect(_x, _y, _width, _height);
+    }
+  }
+}
+
+function drawLandmarks(detections) {
+  if (detections.length > 0) {//If at least 1 face is detected: もし1つ以上の顔が検知されていたら
+    for (f = 0; f < detections.length; f++) {
+      let points = detections[f].landmarks.positions;
+      for (let i = 0; i < points.length; i++) {
+        stroke(44, 169, 225);
+        strokeWeight(3);
+        point(points[i]._x, points[i]._y);
+      }
+    }
+  }
+}
+
+function drawExpressions(detections, x, y, textYSpace) {
+  if (detections.length > 0) {//If at least 1 face is detected: もし1つ以上の顔が検知されていたら
+    let { neutral, happy, angry, sad, disgusted, surprised, fearful } = detections[0].expressions;
+    textFont('Helvetica Neue');
+    textSize(14);
+    noStroke();
+    fill(44, 169, 225);
+
+    text("neutral:       " + nf(neutral * 100, 2, 2) + "%", x, y);
+    text("happiness: " + nf(happy * 100, 2, 2) + "%", x, y + textYSpace);
+    text("anger:        " + nf(angry * 100, 2, 2) + "%", x, y + textYSpace * 2);
+    text("sad:            " + nf(sad * 100, 2, 2) + "%", x, y + textYSpace * 3);
+    text("disgusted: " + nf(disgusted * 100, 2, 2) + "%", x, y + textYSpace * 4);
+    text("surprised:  " + nf(surprised * 100, 2, 2) + "%", x, y + textYSpace * 5);
+    text("fear:           " + nf(fearful * 100, 2, 2) + "%", x, y + textYSpace * 6);
+  } else {//If no faces is detected: 顔が1つも検知されていなかったら
+    text("neutral: ", x, y);
+    text("happiness: ", x, y + textYSpace);
+    text("anger: ", x, y + textYSpace * 2);
+    text("sad: ", x, y + textYSpace * 3);
+    text("disgusted: ", x, y + textYSpace * 4);
+    text("surprised: ", x, y + textYSpace * 5);
+    text("fear: ", x, y + textYSpace * 6);
+  }
+}
