edfc11deca3c57a941251fe5632281b25c5ac54d
jadeye <judmojo@gmail.com>
2022-11-15 14:20:07 +0200

    Revert "Fix table recognition"

--- index.js
+++ index.js
@@ -8,13 +8,14 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const { FaceModel } = require("./models/face.js");
 const bodyParser = require('body-parser');
+const path = require('path');
+const { RecognizedPeople } = require("./models/recognizedPeople.js");
 const { savePersonFace } = require("./services/person.js");
 const { getAllFaces } = require("./services/profilePicture.js");
 const fs = require('fs');
 const cp = require('child_process');
 
 const { getImagesNames } = require("./utils/imagesHelper.js");
-const { saveRecognizedPerson } = require("./services/recognition.js");
 let authorizedPeople = [];
 
 const getBase64StringFromDataURL = (dataURL) =>
@@ -50,7 +51,7 @@ app.post('/user-data', async (req, res) => {
 
   const base64img = getBase64StringFromDataURL(uploaded_file);
   const buffer = Buffer.from(base64img, "base64");
-  const imagePath = `${IMG_PATH}/${name}-${id}.png`;
+  const imagePath = `${IMG_PATH}/${name}.png`;
   fs.writeFileSync(imagePath, buffer);
 
   try {
@@ -81,23 +82,19 @@ app.post('/btn', (req, res) => {
 // TODO: to check if person exists in the arr. if he's so remove him for 15s and send a response back to client. after 15s push him back.
 
 app.post('/detectPeople', async (req, res) => {
-  const { id } = req.body;
+  const { name, id } = req.body;
   const authPerson = authorizedPeople.find((person) => person.id === id); // get the authorized person
+  if (authPerson) {
+    authorizedPeople = authorizedPeople.filter((person) => person.id !== authPerson.id); // remove the auth person from the array
+    const result = await RecognizedPeople.create({ id, name, imagePath: "hardcoded path" });
+    setTimeout(() => {
+      authorizedPeople.push(authPerson);
+    }, 15 * 1000)
 
-  try {
-    if (authPerson) {
-      authorizedPeople = authorizedPeople.filter((person) => person.id !== authPerson.id); // remove the auth person from the array
-      const recognizedPerson = await saveRecognizedPerson(authPerson)
-      setTimeout(() => {
-        authorizedPeople.push(authPerson);
-      }, 15 * 1000)
-
-      res.status(200).json({ success: true, payload: recognizedPerson });
-    } else if (!authPerson) {
-      res.status(400).send({ success: false, message: "Unrecognized person" });
-    }
-  } catch (err) {
-    res.status(500).send({ success: false, message: err });
+    res.status(200).json({ success: true, payload: result });
+  } else if (!authPerson) {
+    replace
+    res.status(400).send({ success: false, message: "Unrecognized person" });
   }
 });
 
@@ -113,6 +110,7 @@ app.get('/getFaces', async (req, res) => {
 
 app.get('/getPhotosNames', (req, res) => {
   const images = getImagesNames();
+
   if (images.length) res.status(200).json(images);
   else res.status(400).json({ success: false, message: "no images in directory" });
 })
@@ -133,6 +131,7 @@ mongoose.connect('mongodb://localhost/recognized_faces')
   .then(async () => {
     console.log('DB Connection eastablished');
     authorizedPeople = await FaceModel.find(); // get all authorized people
+    // console.log(authorizedPeople)
   }).catch((err) => {
     console.error(err);
   });
